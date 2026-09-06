1382870f97b1fdacd03f5004eae7d3580caa91a9
chesszz <14881857+chesszz@users.noreply.github.com>
2026-09-05 21:54:02 -0400

    fix: render songs title on initial load

--- src/pages/+Head.tsx
+++ src/pages/+Head.tsx
@@ -1,10 +1,21 @@
 import { Partytown } from '@builder.io/partytown/react';
 import { Metadata } from '~/components/layout/Metadata';
+import { useTranslation } from 'react-i18next';
+import { usePageContext } from 'vike-react/usePageContext';
 
 export function Head() {
+  const { t } = useTranslation();
+  const { urlPathname } = usePageContext();
+  const basePath = (import.meta.env.BASE_URL ?? '').replace(/\/$/, '');
+  const pagePath = urlPathname.replace(/\/$/, '');
+  const isSongsPage = pagePath === '/songs' || pagePath === `${basePath}/songs`;
+  const title = isSongsPage
+    ? t('title', { titlePrefix: t('songs') })
+    : t('title', { titlePrefix: t('defaultTitlePrefix') });
+
   return (
     <>
-      <Metadata />
+      <Metadata title={title} />
 
       <script
         type="text/partytown"
